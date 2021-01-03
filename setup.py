740da4af5f029c019722c6a1245c89d662b69845
ikarous <ikarous@users.noreply.github.com>
2021-01-03 13:31:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
       name='piptest2',
+      scripts=['test2.py'],
       version='0.1',
       description='pip module testing',
       url='https://github.com/ikarous/piptest2',
       author='William Norris',
       author_email='ike.norris@gmail.com',
       license='MIT',
-      packages=['test2'],
       zip_safe=False
 )
